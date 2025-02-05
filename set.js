5c82f627137bce060ddf6ebdd1666add446cd7d6
Prince-luck <142279296+Prince-luck@users.noreply.github.com>
2025-02-05 12:46:41 +0100

    Check

---
+++ set.js
@@ -0,0 +1,29 @@
+global.prefix = '/'; // Set Your Preferred Prefix
+global.botName = 'ǝunʇɹoɟ'; // Bot Name
+global.botVersion = '1.0.0';
+global.botPic = 'https://files.giftedtech.web.id/file/gifted-md.jpg'; // Can use path, can also use link
+global.botToken = '7498485491:AAHf4P5yAp74eCH83w8jQkSO0ErRa1n_ahU'; // Replace With Your Bot Token (Get it from BotFather)
+global.ownerUsername = 'ǝunʇɹoɟ'; // Replace With Your Telegram Username(For Bot Owner)
+global.timeZone = 'Africa/Lagos'; // Your Time Zone
+global.ownerId = [5918917675]; // Replace With Your Owner ID(For Bot Owner), You Can Add More than 1
+global.footer = '*Powered by ǝunʇɹoɟ*';
+global.ytdlWeb = 'https://ytdl.giftedtech.web.id';
+global.giftedApi = 'https://api.giftedtech.web.id/api'; // Gifted Api
+global.giftedKey = ''; // Gifted Api Key
+global.maherApi = 'https://api.nexoracle.com'; // NexOracle Api
+global.maherKey = ''; // NexOracle Api Key
+global.lyffeApi = 'https://levanter.onrender.com'; // Lyffe Api
+
+global.giftechMess = {
+    internalerr: 'Internal Server Error!',
+    owner: 'Owner-Only Feature!',
+    group: 'Use in a Group!',
+    private: 'Use in a Private Chat!',
+    wait: 'Please Wait...',
+    done: 'Completed Successfully!',
+    error: 'Oops, an Error Occurred'
+};
+
+String.prototype.capitalize = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
